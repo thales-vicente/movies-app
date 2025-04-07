da78d581c4a8c2230652af64f600a3b833b00ab7
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-04-07 18:02:47 -0300

    Add ViewPager2 dependency
    This commit adds the ViewPager2 dependency to the project's build.gradle file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.viewpager2:viewpager2:1.1.0")
 }
