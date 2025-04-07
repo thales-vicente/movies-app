c983cc040a0310b2486d3a4dbb2805387e8891c6
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-04-07 17:54:12 -0300

    Implement Intro, Login and Main Activities
    This commit introduces the Intro, Login, and Main Activities to the app.

---
+++ app/src/main/java/com/example/moviesapp/IntroActivity.kt
@@ -0,0 +1,19 @@
+package com.example.moviesapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.moviesapp.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetStarted.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/example/moviesapp/LoginActivity.kt
+++ app/src/main/java/com/example/moviesapp/LoginActivity.kt
@@ -1,15 +1,30 @@
 package com.example.moviesapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.moviesapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val userName =  binding.etUserName.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (userName.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Please fill your user and password", Toast.LENGTH_SHORT).show()
+            }else if (userName.equals("test") && password.equals("test")){
+                startActivity(Intent(this, MainActivity::class.java))
+            }else{
+                Toast.makeText(this, "Your user or password is not correct", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
